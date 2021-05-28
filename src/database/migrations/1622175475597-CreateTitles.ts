f730bac25e9b4124d641404b523f485e1709d469
VictorG-028 <victor6g0@gmail.com>
2021-05-28 03:06:57 -0300

    Fix Migration 1/2

--- src/database/migrations/1622175475597-CreateTitles.ts
+++ src/database/migrations/1622175475597-CreateTitles.ts
@@ -1,11 +1,11 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class CreateAnimes1620888594082 implements MigrationInterface {
+export class CreateTitles1622175475597 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: "animes",
+                name: "titles",
                 columns: [
                     {
                         name: "id",
@@ -18,30 +18,24 @@ export class CreateAnimes1620888594082 implements MigrationInterface {
                     {
                         name: "titles",
                         type: "varchar",
-                    },
+                    }
+                ],
+                foreignKeys: [
                     {
-                        name: "description",
-                        type: "varchar",
-                        default: "No description provided"
-                    },
-                    {
-                        name: "launch_year",
-                        type: "int",
-                        isNullable: true
-                    },
-                    {
-                        name: "age_limit",
-                        type: "int",
-                        default: 0,
-                        isNullable: true
-                    },
+                        name: "FKAnimes",
+                        referencedTableName: "animes",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["anime_id"],
+                        onDelete: "CASCADE",
+                        onUpdate: "CASCADE"
+                    }
                 ]
             })
         )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("animes");
+        await queryRunner.dropTable("titles");
     }
 
 }
