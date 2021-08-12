e9ad4f2384f8a3bb8e8688b9538c6b240c2afd1b
VictorG-028 <victor6g0@gmail.com>
2021-08-12 12:26:57 -0300

    Refactoring + Creating search url with user input

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,28 @@
-
 // Função usada no popup.html
-
 function togglePopup() {
   document.getElementById("info-popup").classList.toggle("active")
 }
+
+function search(animeMovieQuery) {
+
+  console.log("animeMovieQuery: " + animeMovieQuery);
+
+  const url = `http://localhost:3333/search/?animeMovieQuery=${animeMovieQuery}`;
+  // const animes = get(url);
+  console.log(url);
+}
+
+const textForm = document.getElementById("search-txt");
+const magnifierButton = document.getElementById("search-btn");
+
+textForm.addEventListener("keydown", e => {
+  if (e.code === "Enter") {
+    console.log("enter foi pressionado");
+    search(e.target.value);
+  }
+});
+
+magnifierButton.addEventListener("click", e => { 
+  console.log("button foi clickado");
+  search(textForm.value);
+});
