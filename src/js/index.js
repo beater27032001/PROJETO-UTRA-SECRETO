97f22655255e2a230fb45ec855dae243a495fa05
VictorG-028 <victor6g0@gmail.com>
2021-08-14 15:26:35 -0300

    Test implementation of search feature

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,70 @@
-// Função usada no popup.html
-function togglePopup() {
-  document.getElementById("info-popup").classList.toggle("active")
-}
+class Utils {
 
-function search(animeMovieQuery) {
-
-  console.log("animeMovieQuery: " + animeMovieQuery);
-
-  const url = `http://localhost:3333/search/?animeMovieQuery=${animeMovieQuery}`;
-  // const animes = get(url);
-  console.log(url);
-}
-
-const textForm = document.getElementById("search-txt");
-const magnifierButton = document.getElementById("search-btn");
-
-textForm.addEventListener("keydown", e => {
-  if (e.code === "Enter") {
-    console.log("enter foi pressionado");
-    search(e.target.value);
+	static getAnimes = (animeMovieQuery) => {
+		return new Promise((resolve, reject) => {
+			const url = `http://localhost:3333/search/?animeMovieQuery=${animeMovieQuery}`;
+			let response = fetch(url)
+			.then( response => {
+				  // console.log("Promised response");
+				  // console.log(response);
+				  return response.json();
+			})
+			.then( animes => {
+					// console.log("Data");
+				  // console.log(animes);
+				  resolve(animes);
+			});
+		});
+	}
+  
   }
-});
-
-magnifierButton.addEventListener("click", e => { 
-  console.log("button foi clickado");
-  search(textForm.value);
-});
+  
+  // Função usada no popup.html
+  function togglePopup() {
+	document.getElementById("info-popup").classList.toggle("active")
+  }
+  
+  async function search(animeMovieQuery) {
+	  console.log("animeMovieQuery: " + animeMovieQuery);
+  
+	  const animes = await Utils.getAnimes(animeMovieQuery);
+  
+	  console.log("Actual response")
+	  console.log(animes);
+	  
+	  let i = 0;
+  
+	  let HTML = "";
+	  animes.forEach( anime => {
+		  if (i < 4) {
+			  HTML += `<div class="owl-item active" style="width: 239.4px; margin-right: 10px;>`;
+		  } else {
+			  HTML += `<div class="owl-item" style="width: 239.4px; margin-right: 10px;>`;
+		  }
+		  HTML += `<div class="item">`;
+		  HTML += `<img class="box-filme" src="../img/m1.jpg">`;
+		  HTML += `</div>`;
+		  HTML += `</div>`;
+  
+		  i++;
+	  });
+  
+	  const owlStageOuter = document.querySelector(".owl-stage");
+	  owlStageOuter.innerHTML = HTML;
+  }
+  
+  const textForm = document.getElementById("search-txt");
+  const magnifierButton = document.getElementById("search-btn");
+  
+  textForm.addEventListener("keydown", e => {
+	if (e.code === "Enter") {
+	  console.log("enter foi pressionado");
+	  search(e.target.value);
+	}
+  });
+  
+  magnifierButton.addEventListener("click", e => { 
+	console.log("button foi clickado");
+	search(textForm.value);
+  });
+  
