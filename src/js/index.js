e629796a00d67695a51901e768848b5b6e5a5cd8
VictorG-028 <victor6g0@gmail.com>
2021-08-17 22:56:09 -0300

    Refactoring index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,54 +1,16 @@
 // Função usada no popup.html
 function togglePopup() {
-	document.getElementById("info-popup").classList.toggle("active")
+	document.getElementById("info-popup").classList.toggle("active");
 }
-  
-async function search(animeMovieQuery) {
-	console.log("animeMovieQuery: " + animeMovieQuery);
-  
-	const animes = await Utils.getAnimes(animeMovieQuery);
-  
-	console.log("Actual response")
-	console.log(animes);
-	  
-	let i = 0;
-  
-	let HTML = "";
-	animes.forEach( anime => {
-		// const mainTitle = anime.title[0];
 
-		if (i < 4) {
-			HTML += `<div class="owl-item active" style="width: 239.4px; margin-right: 10px;">\n`;
-		} else {
-			HTML += `<div class="owl-item" style="width: 239.4px; margin-right: 10px;">\n`;
-		}
-		HTML += `<div class="item">\n`;
-		HTML += `<img class="box-filme" src="../img/${anime.title}.jpg">\n`;
-		HTML += `</div>\n`;
-		HTML += `</div>\n`;
-  
-		i++;
-	});
-  
-	const owlStageOuter = document.querySelector(".owl-stage");
-	owlStageOuter.innerHTML = HTML;
 
-	const carroselFilmes = document.querySelector(".carrosel-filmes");
-	carroselFilmes.style = "display: block;";
-}
-  
-const textForm = document.getElementById("search-txt");
-const magnifierButton = document.getElementById("search-btn");
-  
-textForm.addEventListener("keydown", e => {
-	if (e.code === "Enter") {
-		console.log("enter foi pressionado");
-		search(e.target.value);
-	}
-});
-  
-magnifierButton.addEventListener("click", e => { 
-	console.log("button foi clickado");
-	search(textForm.value);
-});
+// function openAnime(title) {
+
+// }
+
+
+// const animes = document.querySelectorAll(".item");
+// animes.forEach(anime => {
+// 	anime.addEventListener("click", openAnime(title));
+// });
   
